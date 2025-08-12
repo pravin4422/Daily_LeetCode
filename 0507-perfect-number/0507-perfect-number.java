d561d45056e361be3a728af4fd46be2da06a0e9d
Pravin.R <rpravin4422@gmail.com>
2025-08-12 09:58:53 +0530

    Time: 2095 ms (33.99%), Space: 40.8 MB (19.8%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int count =0;
+        for(int i =1; i<num;i++){
+            if(num%i==0){
+                count+=i;
+            }
+        }
+        if(count==num){
+            return true;
+        }
+        return false;
+    }
+}
